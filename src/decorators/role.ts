a54ca17284d4ab1b9e5070af10a75149e29e7443
Jevgeni Sillar <jevgeni@pitfire.io>
2021-09-12 14:52:26 +0300

    export new things

--- src/decorators/role.ts
+++
@@ -1,9 +0,0 @@
-export const Role = (roles: string[]) => {
-  return (target, propertyKey, descriptor) => {
-    if ((target['methods'] = target['methods'] || {})[propertyKey]) {
-      (target['methods'] = target['methods'] || {})[propertyKey].roles = roles;
-    }
-
-    return descriptor;
-  };
-};
